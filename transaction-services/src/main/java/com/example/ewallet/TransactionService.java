496e359aecdc2b46f7aace96dcc33050972edaf4
hamdtc <mohamedtc73@gmail.com>
2023-01-08 07:06:53 +0530

    final

--- transaction-services/src/main/java/com/example/ewallet/TransactionService.java
+++ transaction-services/src/main/java/com/example/ewallet/TransactionService.java
@@ -4,10 +4,15 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.Date;
 import java.util.UUID;
 
@@ -22,6 +27,9 @@ public class TransactionService {
 
     @Autowired
     ObjectMapper objectMapper;
+
+    @Autowired
+    RestTemplate restTemplate;
 
     public void createTransaction(TransactionRequest transactionRequest){
 
@@ -51,16 +59,57 @@ public class TransactionService {
     @KafkaListener(topics={"update_transaction"},groupId="avengers")
     public void updateTransaction(String message) throws JsonProcessingException {
         JSONObject transactionRequest = objectMapper.readValue(message, JSONObject.class);
+        String fromUser= (String) transactionRequest.get("fromUser");
+        String toUser= (String) transactionRequest.get("toUser");
 
         String status = (String) transactionRequest.get("status");
         String transactionId = (String) transactionRequest.get("transactionId");
+        Transaction transaction=transactionRepository.findByTransactionId(transactionId);
 
-        Transaction transaction = transactionRepository.findByTransactionId(transactionId);
+        transaction.setFromUser(fromUser);
+        transaction.setToUser(toUser);
 
         if(status=="SUCCESS")
             transaction.setStatus(TransactionStatus.SUCCESS);
         else transaction.setStatus(TransactionStatus.FAILED);
 
         transactionRepository.save(transaction);
+        callNotificationService(transaction);
+    }
+
+
+    public void callNotificationService(Transaction transaction){
+
+        String fromUser = transaction.getFromUser();
+        String toUser = transaction.getToUser();
+
+        URI url = URI.create("http://localhost:8076/user?userName="+fromUser);
+        HttpEntity httpEntity = new HttpEntity(new HttpHeaders());
+        JSONObject sender = restTemplate.exchange(url, HttpMethod.GET,httpEntity, JSONObject.class).getBody();
+        String senderName = (String) sender.get("name");
+        String senderEmail = (String) sender.get("email");
+
+        url = URI.create("http://localhost:8076/user?userName="+toUser);
+        JSONObject receiver = restTemplate.exchange(url, HttpMethod.GET,httpEntity, JSONObject.class).getBody();
+        String receiverName = (String) receiver.get("name");
+        String receiverEmail = (String) receiver.get("email");
+
+        // kafka
+        JSONObject senderEmailRequest = new JSONObject();
+        senderEmailRequest.put("email",senderEmail);
+        String senderMessage = String.format("Hi %s. Your transaction with transactionId %s to %s of amount %d is in %s status.",senderName,transaction.getTransactionId(),receiverName,transaction.getAmount(),transaction.getStatus());
+        senderEmailRequest.put("message",senderMessage);
+        String message = senderEmailRequest.toString();
+        kafkaTemplate.send("send_email",message);
+
+        if(transaction.getStatus()==TransactionStatus.FAILED)
+            return;
+
+        JSONObject receiverEmailRequest = new JSONObject();
+        receiverEmailRequest.put("email",receiverEmail);
+        String receiverMessage = String.format("Hi %s. You have received %d rupees from %s",receiverName,transaction.getAmount(),senderName);
+        receiverEmailRequest.put("message",receiverMessage);
+        message = receiverEmailRequest.toString();
+        kafkaTemplate.send("send_email",message);
     }
 }
--- wallet-services/src/main/java/com/example/ewallet/WalletConfig.java
+++ wallet-services/src/main/java/com/example/ewallet/WalletConfig.java
@@ -17,72 +17,73 @@ import org.springframework.kafka.core.*;
 import java.util.Properties;
 @Configuration
 public class WalletConfig {
-    @Bean
-    LettuceConnectionFactory getConnection(){
+//    @Bean
+//    LettuceConnectionFactory getConnection(){
+//
+//        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+//        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
+//        return lettuceConnectionFactory;
+//    }
+//
+//    @Bean
+//    RedisTemplate<String,Object> redisTemplate(){
+//
+//        RedisTemplate<String,Object> redisTemplate = new RedisTemplate<>();
+//
+//        // key //serializer
+//        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+//        redisTemplate.setKeySerializer(redisSerializer);
+//
+//        // value
+//        JdkSerializationRedisSerializer jdkSerializationRedisSerializer = new JdkSerializationRedisSerializer();
+//        redisTemplate.setValueSerializer(jdkSerializationRedisSerializer);
+//        redisTemplate.setHashValueSerializer(jdkSerializationRedisSerializer);
+//
+//        redisTemplate.setConnectionFactory(getConnection());
+//
+//        return redisTemplate;
+//    }
 
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
-        return lettuceConnectionFactory;
-    }
+//    @Bean
+//    ObjectMapper objectMapper(){
+//        return new ObjectMapper();
+//    }
 
-    @Bean
-    RedisTemplate<String,Object> redisTemplate(){
-
-        RedisTemplate<String,Object> redisTemplate = new RedisTemplate<>();
-
-        // key //serializer
-        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
-        redisTemplate.setKeySerializer(redisSerializer);
-
-        // value
-        JdkSerializationRedisSerializer jdkSerializationRedisSerializer = new JdkSerializationRedisSerializer();
-        redisTemplate.setValueSerializer(jdkSerializationRedisSerializer);
-        redisTemplate.setHashValueSerializer(jdkSerializationRedisSerializer);
-
-        redisTemplate.setConnectionFactory(getConnection());
-
-        return redisTemplate;
-    }
-
-    @Bean
-    ObjectMapper objectMapper(){
-        return new ObjectMapper();
-    }
-
-    @Bean
-    Properties kafkaProperties(){
-        Properties properties = new Properties();
-
-        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class);
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
-
-        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringSerializer.class);
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
-
-        return properties;
-    }
-
-    @Bean
-    ConsumerFactory<String,String> getConsumerFactory(){
-        return new DefaultKafkaConsumerFactory(kafkaProperties());
-    }
-
-    @Bean
-    ConcurrentKafkaListenerContainerFactory<String,String> concurrentKafkaListenerContainerFactory(){
-        ConcurrentKafkaListenerContainerFactory concurrentKafkaListenerContainerFactory = new ConcurrentKafkaListenerContainerFactory();
-        concurrentKafkaListenerContainerFactory.setConsumerFactory(getConsumerFactory());
-        return concurrentKafkaListenerContainerFactory;
-    }
-
-    @Bean
-    ProducerFactory<String,String> getProducerFactory(){
-        return new DefaultKafkaProducerFactory(kafkaProperties());
-    }
-
-    @Bean
-    KafkaTemplate<String,String> getKafkaTemplate(){
-        return new KafkaTemplate<>(getProducerFactory());
-    }
+//    @Bean
+//    Properties kafkaProperties(){
+//        Properties properties = new Properties();
+//
+//        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class);
+//        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+//
+//        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringSerializer.class);
+//        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+//
+//        return properties;
+//    }
+//
+//    @Bean
+//    ConsumerFactory<String,String> getConsumerFactory(){
+//        return new DefaultKafkaConsumerFactory(kafkaProperties());
+//    }
+//
+//    @Bean
+//    ConcurrentKafkaListenerContainerFactory<String,String> concurrentKafkaListenerContainerFactory(){
+//        ConcurrentKafkaListenerContainerFactory concurrentKafkaListenerContainerFactory = new ConcurrentKafkaListenerContainerFactory();
+//        concurrentKafkaListenerContainerFactory.setConsumerFactory(getConsumerFactory());
+//        return concurrentKafkaListenerContainerFactory;
+//    }
+//
+//    @Bean
+//    ProducerFactory<String,String> getProducerFactory(){
+//        return new DefaultKafkaProducerFactory(kafkaProperties());
+//    }
+//
+//    @Bean
+//    KafkaTemplate<String,String> getKafkaTemplate(){
+//        return new KafkaTemplate<>(getProducerFactory());
+//    }
+//}
 }
